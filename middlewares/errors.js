e896244da4b924fab9380b0bfbb55084b89b4f89
hussamkhatib <hussamkhatib@gmail.com>
2022-09-27 20:45:47 +0530

    handler errors differently based on env

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -1,23 +1,24 @@
+require("dotenv").config();
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
 
-  //   if (process.env.NODE_ENV === "development") {
-  res.status(err.statusCode).json({
-    success: false,
-    error: err,
-    errMessage: err.message,
-    stack: err.stack,
-  });
-  //   }
+  if (process.env.NODE_ENV === "development") {
+    res.status(err.statusCode).json({
+      success: false,
+      error: err,
+      errMessage: err.message,
+      stack: err.stack,
+    });
+  }
 
-  //   if (process.env.NODE_ENV === "production ") {
-  //     let error = { ...err };
+  if (process.env.NODE_ENV === "production") {
+    let error = { ...err };
 
-  //     error.message = err.message;
+    error.message = err.message;
 
-  //     res.status(error.statusCode).json({
-  //       success: false,
-  //       message: error.message || "Internal Server Error.",
-  //     });
-  //   }
+    res.status(error.statusCode).json({
+      success: false,
+      message: error.message || "Internal Server Error.",
+    });
+  }
 };
